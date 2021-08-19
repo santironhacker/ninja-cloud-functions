06af10baa1b50f918c3f913ae6d8bc3ab06e587b
Santiago Ramos <santiago.ramos@serdao.fr>
2021-08-19 14:56:56 +0200

    Created randomNumber and toTheDojo cloud functions.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,17 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Deploy command
+// Use: firebase deploy --only functions
+
+// http request 1
+// https.onRequest will generate like an endpoint we can reach out from the front
+exports.randomNumber = functions.https.onRequest((request, response) => {
+	const number = Math.round(Math.random() * 100);
+	console.log("random number", number);
+	response.send(number.toString());
+});
+
+exports.toTheDojo = functions.https.onRequest((request, response) => {
+	// response.redirect('https://www.thenetninja.co.uk');
+	response.redirect("https://www.codewars.com");
+});
