d550efbfc2f7cead7977ccfd7d6d7138a5a8c5f1
Santiago Ramos <santiago.ramos@serdao.fr>
2021-08-19 14:56:29 +0200

    Updated package.json. Added eslint rules and eslintignore file.

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
+	root: true,
+	env: {
+		es6: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"google",
+	],
+	rules: {
+		"quotes": ["error", "double"],
+		"indent": [2, "tab"],
+		"no-tabs": 0,
+		"max-len": 0,
+	},
 };
