7f5bad049bf399cc91805e7a9ef1095b7f643238
Santiago Ramos <santiago.ramos@serdao.fr>
2021-10-01 18:06:54 +0700

    [Creating User Records] Added newUserSignUp and userDeleted functions that interact with the Firebase DB.

--- functions/index.js
+++ functions/index.js
@@ -22,3 +22,22 @@ exports.sayHello = functions.https.onCall((data, context) => {
 	const user = data.name;
 	return `Hello ${user}`;
 });
+
+/* USER AUTH LISTENERS */
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+	// for background triggers you must return a value/promise
+	return admin.firestore().collection("users").doc(user.uid).set({
+		email: user.email,
+		upvotedOn: [],
+	});
+});
+
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+	const doc = admin.firestore().collection("users").doc(user.uid);
+	return doc.delete();
+});
