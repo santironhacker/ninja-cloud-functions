c8d36fff7c77898a1eabcb819b109a4d52d6b63b
Santiago Ramos <santiago.ramos@serdao.fr>
2021-10-02 10:58:49 +0700

    [Addind new requests] Added addRequest function with  http.onCall to send a new request.

--- functions/index.js
+++ functions/index.js
@@ -41,3 +41,24 @@ exports.userDeleted = functions.auth.user().onDelete((user) => {
 	const doc = admin.firestore().collection("users").doc(user.uid);
 	return doc.delete();
 });
+
+// http callable function (adding a request)
+exports.addRequest = functions.https.onCall((data, context) => {
+	if (!context.auth) {
+		throw new functions.https.HttpsError(
+			"unauthenticated", // this is an error code from the list https://firebase.google.com/docs/reference/functions/providers_https_#functionserrorcode
+			"only authenticated users can add requests"
+		);
+	}
+	if (data.text.length > 30) {
+		throw new functions.https.HttpsError(
+			"invalid-argument",
+			"request must be no more than 30 characters long"
+		);
+	}
+	// when we call we need to return something to the user
+	return admin.firestore().collection("requests").add({
+		text: data.text,
+		upvotes: 0,
+	});
+});
